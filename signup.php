233225a37dcbccda2e90c9f17c736da02ea75d24
durbi009 <durbi009@cougars.csusm.edu>
2021-05-05 20:43:50 -0700

    ;)

--- signup.php
+++ signup.php
@@ -3,13 +3,11 @@
 include'connect.php';
 $conn = OpenCon();
 
-if (isset($_POST['submit']))
-    {
     $username = filter_input(INPUT_POST,'username');
     $password = filter_input(INPUT_POST,'password');
     $email = filter_input(INPUT_POST, 'email');
     $INSERT = "INSERT INTO `users` (`email`, `username`, `password`) VALUES ('$email','$username','$password')";
-    }
+
 if ($conn->query($sql))
 {
     echo "New record added!";
