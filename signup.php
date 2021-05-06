70fedb5b9456ce0ce65dad0090ad0acf8180c121
durbi009 <durbi009@cougars.csusm.edu>
2021-05-05 17:08:13 -0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,24 +4,22 @@ include'connect.php';
 $conn = OpenCon();
 
 if (isset($_POST['submit']))
-{
+    {
     $username = filter_input(INPUT_POST,'username');
     $password = filter_input(INPUT_POST,'password');
     $email = filter_input(INPUT_POST, 'email');
     $INSERT = "INSERT INTO `users` (`email`, `username`, `password`) VALUES ('$email','$username','$password')";
-
-    if ($conn->query($sql))
-        {
-        echo "New record added!";
-        }
-    elseif($conn->connect_error)
-        {
-        die('Could not connect to database');
-        }
-}
-else
+    }
+if ($conn->query($sql))
 {
-echo "Sumbit button not set";
+    echo "New record added!";
 }
+elseif($conn->connect_error)
+{
+    echo('Could not connect to database');
+    die();
 }
+    
+
+
 ?>
