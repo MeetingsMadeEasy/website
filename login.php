bbf63c21197f0f4817a160a51272729077b14eec
durbi009 <durbi009@cougars.csusm.edu>
2021-05-08 20:21:42 -0700

    TRYING TO GET LOGIN.PHP TO WORK

--- login.php
+++ login.php
@@ -3,5 +3,32 @@
 include'connect.php';
 $conn = OpenCon();
 
+if(isset($_POST['submitButton'])){
+	$email = $_POST['email'];
+	$pass = $_POST['password'];
+
+	$email = stripcslashes($email);  
+    $pass = stripcslashes($pass);  
+    $email = mysqli_real_escape_string($conn, $email);  
+    $pass = mysqli_real_escape_string($conn, $pass);
+
+    $sql = "SELECT *from users where email = '$email' and pass_word = '$pass'";  
+    $result = mysqli_query($conn, $sql);  
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+    $count = mysqli_num_rows($result); 
+
+    if($count == 1){
+    	$_SESSION['email'] = $email;
+        echo("LOGIN SUCCESSFUL");
+        //echo $row['first_name'];
+    header("Location:homepage.html");
+    }
+    else
+    {
+    	echo "Login failed. Invalid username or password";
+        echo "<script>setTimeout(\"location.href = 'login.php';\", 3000); </script>";
+        exit();
+    }
+}
 
 ?>
