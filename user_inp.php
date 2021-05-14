98846e3281c3fead672dc7a5b83007aacdd503ff
durbi009 <durbi009@cougars.csusm.edu>
2021-05-13 20:33:33 -0700

    :)

--- user_inp.php
+++ user_inp.php
@@ -5,7 +5,7 @@ $conn = OpenCon();
 $MeetingName = filter_input(INPUT_POST,'ZoomUser');
 $MeetingID = filter_input(INPUT_POST,'meetingid');
 $MeetingPswd = filter_input(INPUT_POST,'password');
-$INSERT = "INSERT INTO 'meetings' ('name','id','password') VALUES ('$MeetingName','$MeetingID','$MeetingPswd')";
+
 $sql = "INSERT INTO `meetings`(`name`, `id`, `password`) VALUES ('$MeetingName','$MeetingID','$MeetingPswd')";
 if ($conn->query($sql))
 {
