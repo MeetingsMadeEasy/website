53a7fcff6770c456f628d79745642ecb48b2be04
durbi009 <durbi009@cougars.csusm.edu>
2021-05-13 20:13:26 -0700

    :)

--- user_inp.php
+++ user_inp.php
@@ -7,4 +7,16 @@ $MeetingID = filter_input(INPUT_POST,'meetingid');
 $MeetingPswd = filter_input(INPUT_POST,'password');
 $INSERT = "INSERT INTO 'meetings' ('name','id','password') VALUES ('$MeetingName','$MeetingID','$MeetingPswd')";
 
+if ($conn->query($INSERT))
+{
+echo "New record added!";
+}
+elseif($conn->connect_error)
+{
+echo "Could not connect to database";
+die();
+}
+else{
+ echo "hi";  
+}
 ?>
