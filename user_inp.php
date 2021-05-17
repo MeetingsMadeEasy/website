527895921353eff5357bf0cfe83f3a48fde4637c
kucha004 <kucha004@cougars.csusm.edu>
2021-05-16 22:54:17 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/MeetingsMadeEasy/website into main""
    This reverts commit ed8f10cc6379852121f2445804e57cd2f03984c3.

--- user_inp.php
+++ user_inp.php
@@ -5,6 +5,18 @@ $conn = OpenCon();
 $MeetingName = filter_input(INPUT_POST,'ZoomUser');
 $MeetingID = filter_input(INPUT_POST,'meetingid');
 $MeetingPswd = filter_input(INPUT_POST,'password');
-$INSERT = "INSERT INTO 'meetings' ('name','id','password') VALUES ('$MeetingName','$MeetingID','$MeetingPswd')";
 
+$sql = "INSERT INTO `meetings`(`name`, `id`, `password`) VALUES ('$MeetingName','$MeetingID','$MeetingPswd')";
+if ($conn->query($sql))
+{
+echo "New record added!";
+}
+elseif($conn->connect_error)
+{
+echo "Could not connect to database";
+die();
+}
+else{
+ echo "did not work";  
+}
 ?>
