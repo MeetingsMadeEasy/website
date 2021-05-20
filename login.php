4f28a61a3eecd36bfda68f77aee70b5af9ee4e52
durbi009 <durbi009@cougars.csusm.edu>
2021-05-20 00:07:28 -0700

    reverted to previous working version

--- login.php
+++ login.php
@@ -1,7 +1,29 @@
 <?php
 
+session_start();
+
 include'connect.php';
 $conn = OpenCon();
 
+if(isset($_POST['Login'])){
+	//$email = $_POST['email'];
+	//$pass = $_POST['password'];
+    $username = filter_input(INPUT_POST,'username');
+    $password = filter_input(INPUT_POST,'password');
 
+    $sql = "SELECT *from users where username = '$username' and password = '$password'";  
+    $result = mysqli_query($conn, $sql);  
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+    $count = mysqli_num_rows($result); 
+
+    if($count == 1){
+    	$_SESSION['username'] = $username;
+        header("Location:homepage.html");
+    }
+    else
+    {
+    	echo "Login failed. Invalid username or password. Try Again?";
+        header("Location.login.html");
+    }
+}
 ?>
